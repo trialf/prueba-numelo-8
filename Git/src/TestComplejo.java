09bfe13f4eaed380a0fe5abe948377cc59c842d2
afoncubierta <afoncubierta@106-14.info2>
2012-05-21 12:04:28 +0200

    aeiopsdb

---
+++ Git/src/TestComplejo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class TestComplejo {
+	
+
+
+
+	public static void main(String[] args) {
+		Scanner snc= new Scanner(System.in);
+		Complejo prueba=new Complejo();
+		prueba.seti(10);
+		prueba.setj(5);
+		prueba.mostrar("El numero complejo es: ", prueba);
+		System.out.println("-------------------------");
+		
+		Complejo pro = new Complejo(2,4);
+		//pro.seti(4);
+		//pro.setj(4);
+		pro.mostrar("El nuevo complejo es: ", pro);
+		pro.mostrar("El producto de prueba y pro es:", pro.getproducto(prueba));
+		System.out.println("--------------------------");
+		Complejo div = new Complejo(1,6);
+		div.seti(1);
+		div.setj(6);
+		div.seti(snc.nextDouble());
+		div.mostrar("La division es:", div.getDivision(prueba));
+	
+	}
+}
